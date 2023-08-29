8583d0727428bbe3f677754dc85f3b33c1fc7edf
Emil <emil@webform.se>
2023-08-29 11:48:14 +0200

    progress

--- 1 - Python/Obligatorisk/main.py
+++ 1 - Python/Obligatorisk/main.py
@@ -2,9 +2,21 @@
 #import module
 import pandas as pd
 
-
-mandatory, results = pd.read_csv((r"Git---interaktiv\1 - Python\Obligatorisk\ObligatoriskaMediaKurser.csv"), names= 
+"""Import data"""
+media_courses, results = pd.read_csv((r"Git---interaktiv\1 - Python\Obligatorisk\ObligatoriskaMediaKurser.csv"), names= 
                        ["Course_code","Course_name", "Points"], sep=";"), pd.read_csv((r"Git---interaktiv\1 - Python\Obligatorisk\Studieresultat.csv"), sep=";", names= [i for i in range(17)])
                     
-#print(mandatory.Course_code)
-print(results)
+#print(media_courses)
+#print(results)
+
+def media_student(student_courses, media_courses):
+        if student_courses in media_courses:
+            return True 
+        return False
+
+for row in media_courses.iloc:
+    print(row.Course_code)
+    
+#if media_student:
+    
+
